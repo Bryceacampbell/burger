6135ab07695a2b4934c54916ffb880118325c0da
Bryce's Laptop <bryceacampbell7@gmail.com>
2019-05-20 23:15:36 -0700

    adding updates

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -16,4 +16,25 @@ $(document).ready(function () {
             location.reload();
         });
     });
+
+    $(".devour-btn").on("click", function (event) {
+        
+        let id = $(this).data("id");
+        let newDevoured = $(this).data("newdevoured");
+        newDevoured = {
+            devoured: 1
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevoured
+        }).then(
+            function () {
+                console.log("changed Devoured to: ", newDevoured);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
 });
